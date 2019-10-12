6ada017a2164c906ac54aec03173f4222d06b508
Ethan Anderson <ethan.anderson00@gmail.com>
2019-10-12 17:44:20 +0100

    Added converter function and package.json

---
+++ index.js
@@ -0,0 +1,24 @@
+let converter = (input) => {
+  let uppercase = true;
+  let convertedString = "";
+
+  for (let i = 0; i < input.length; i++) {
+    let character = input.charAt(i);
+
+    if (uppercase === true) {
+      character = character.toUpperCase();
+      convertedString = convertedString + character;
+      uppercase = false;
+    } else {
+      character = character.toLowerCase();
+      convertedString = convertedString + character;
+      uppercase = true;
+    }
+
+    if (convertedString.length === input.length) {
+      return convertedString;
+    }
+  }
+};
+
+module.exports = converter;
